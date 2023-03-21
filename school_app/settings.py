26153e7debf59f5f251dd1efe1e4c3238a5544c5
mbubux <mbubux@gmail.com>
2023-03-21 14:08:01 +0530

    DB Updated by Rakibul

--- school_app/settings.py
+++ school_app/settings.py
@@ -84,23 +84,23 @@ WSGI_APPLICATION = "school_app.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-#     }
-# }
-
 DATABASES = {
-    'default': {
-		'ENGINE': 'django.db.backends.mysql',
-		'NAME': 'MANAS_ACADEMY',
-		'USER': 'root',
-		'PASSWORD': '123456',
-		'HOST':'localhost',
-		'PORT':'3306',	
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    }
 }
-}
+
+# DATABASES = {
+#     'default': {
+# 		'ENGINE': 'django.db.backends.mysql',
+# 		'NAME': 'MANAS_ACADEMY',
+# 		'USER': 'root',
+# 		'PASSWORD': '123456',
+# 		'HOST':'localhost',
+# 		'PORT':'3306',	
+# }
+# }
 
 
 # Password validation
